e2948e8fd4fe3b1d4d07abc13933ae5c14869a9e
Aldo Torres <atorres6d5@gmail.com>
2017-10-31 09:34:59 -0700

    made first app page, and err handels

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const uuid = require('uuid/v4')
+
+
+
+app.use(bodyParser.json())
+
+const routes = require('./routes/routes.js')
+
+app.use('/books', routes)
+
+app.use((err, req, res, next) =>{
+  const status = err.status || 500
+  res.status(status).json({error: err})
+})
+
+app.use((req, res, next) => {
+  res.status(404).json({error: {message: 'Not Found'}})
+})
+
+const listener = () => `listening on port ${port}!`
