7a00224b26e21e2a152e42c411dc701aac51f097
Aldo Torres <atorres6d5@gmail.com>
2017-11-01 11:53:46 -0700

    added start script/

--- app.js
+++ app.js
@@ -13,14 +13,20 @@ app.use(morgan('dev'))
 
 app.use('/books', routes)
 
-app.use((err, req, res, next) =>{
+app.use((err, req, res, next) => {
   console.log(err)
   const status = err.status || 500
-  res.status(status).json({error: err})
+  res.status(status).json({
+    error: err
+  })
 })
 
 app.use((req, res, next) => {
-  res.status(404).json({error: {message: 'Not Found'}})
+  res.status(404).json({
+    error: {
+      message: 'Not Found'
+    }
+  })
 })
 
 const listener = () => `listening on port ${port}!`
--- ctrl/ctrl.js
+++ ctrl/ctrl.js
@@ -1,69 +1,115 @@
 const model = require('../model/model.js')
 
 
-function getAll(req, res, next){
+function getAll(req, res, next) {
   let collection = model.getAll()
   //console.log(collection)
-  return res.status(200).send({ data: collection})
+  return res.status(200).send({
+    data: collection
+  })
 }
 
-function getOne(req, res, next){
+function getOne(req, res, next) {
   let onebook = model.getOne(req.params.id)
-  return res.status(200).send({data: onebook})
+  return res.status(200).send({
+    data: onebook
+  })
 }
 
-function makeBook(req, res, next){
+function makeBook(req, res, next) {
   let body = req.body
-  if(!body.title || !body.first || !body.last){
-    return next({status:400, message: "Must provide book title, and authors first and last name"})
+  if (!body.title || !body.first || !body.last) {
+    return next({
+      status: 400,
+      message: "Must provide book title, and authors first and last name"
+    })
   }
   let thisBook = model.newBook(body.title, body.first, body.last)
-  return res.status(200).send({data:thisBook})
+  return res.status(200).send({
+    data: thisBook
+  })
 }
 
-function update(req, res, next){
+function update(req, res, next) {
   let id = req.params.id
   let Description = req.body.info
 
-  if(!id || !Description) return next({status: 400, message:"please provide us with more information"})
+  if (!id || !Description) return next({
+    status: 400,
+    message: "please provide us with more information"
+  })
   //console.log(Description);
   let relevant = model.upDate(id, Description)
 
-   //console.log(relevant);
-  if(!relevant) return next({status:404, message: "no book found with that name"})
-  return res.status(200).send({data: relevant})
+  //console.log(relevant);
+  if (!relevant) return next({
+    status: 404,
+    message: "no book found with that name"
+  })
+  return res.status(200).send({
+    data: relevant
+  })
 }
 
-function remove(req, res, next){
+function remove(req, res, next) {
   let id = req.params.id
-  if(!id) return next({status: 400, message: "What book do you want to delete?"})
+  if (!id) return next({
+    status: 400,
+    message: "What book do you want to delete?"
+  })
   let lostBook = model.lostBook(id)
-  if(lostBook) lostBook="Book Removed"
-  return res.status(200).send({data:lostBook})
+  if (lostBook) lostBook = "Book Removed"
+  return res.status(200).send({
+    data: lostBook
+  })
 }
 
-function getEveryone(req, res, next){
+function getEveryone(req, res, next) {
   let id = req.params.id
   let Authors = model.getEveryone(id)
-  if (!Authors) return next({status: 400, message: "needs a matching id"})
-  return res.status(200).send({data: Authors})
-  }
+  if (!Authors) return next({
+    status: 400,
+    message: "needs a matching id"
+  })
+  return res.status(200).send({
+    data: Authors
+  })
+}
 
-function addAuthor(req, res, next){
+function addAuthor(req, res, next) {
   let id = req.params.id
   let body = req.body
   let newAuthors = model.newAuthors(id, body)
   //console.log(newAuthors)
-  if (!newAuthors) return next({status: 404, message: "need a matching id"})
-  return res.status(200).send({data: newAuthors})
+  if (!newAuthors) return next({
+    status: 404,
+    message: "need a matching id"
+  })
+  return res.status(200).send({
+    data: newAuthors
+  })
 }
 
-function removeAuthor(req, res, next){
+function removeAuthor(req, res, next) {
   let bookID = req.params.bookID
   let autherID = req.params.authorID
   let nix = model.removeAuthor(bookID, autherID)
-  if (!nix) next({status: 400, message: "did not delete you Author"})
-  if(nix) return res.status(200).send({message:"author removed from book"})
+  if (!nix) next({
+    status: 400,
+    message: "did not delete you Author"
+  })
+  if (nix) return res.status(200).send({
+    message: "author removed from book"
+  })
 }
 
-module.exports = { getAll, getOne, makeBook, update, remove, getEveryone, addAuthor, removeAuthor}
+module.exports = {
+  getAll,
+  getOne,
+  makeBook,
+  update,
+  remove,
+  getEveryone,
+  addAuthor,
+  removeAuthor
+}
